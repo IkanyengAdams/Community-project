e770176b1f1761aa3bcc569bd27c9adf674e101c
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-03-03 22:42:42 +0200

    modified html

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        submitForm();
+    });
+  
+    function submitForm() {
+        const name = document.getElementById('name').value;
+        const age = document.getElementById('age').value;
+        const gender = document.getElementById('gender').value;
+        const healthCondition = document.getElementById('healthCondition').value;
+  
+        
+        if (!name || !age || !gender || !healthCondition) {
+            alert('Please fill in all fields');
+            return;
+        }
+  
+        // Redirect to another page
+        window.location.href = 'confirmation.html';
+    }
+  });
+  
